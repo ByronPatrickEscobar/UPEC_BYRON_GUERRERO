08296146efa1ef2b75b84dcd26b5afe3f91eff3d
Byron1980 <69835056+Byron1980@users.noreply.github.com>
2020-08-22 20:04:56 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>PROYECTO UPEC BYRON GUERRERO </h1>
+      <h2>DA CLICK PARA INICIAR</h2>
+    </div>
+  );
+}
